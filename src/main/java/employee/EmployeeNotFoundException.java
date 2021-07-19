02a4196ff6fa1896affeb97b3ddb8da78b1abdff
Michael Mörtenhuber <moertenmic@gmail.com>
2021-07-19 14:29:37 +0200

    Removed tutorial code. whoopsie!

--- src/main/java/employee/EmployeeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package employee;
-
-class EmployeeNotFoundException extends RuntimeException {
-
-    EmployeeNotFoundException(Long id) {
-        super("Could not find employee " + id);
-        // Evokes a HTTP 404 by default by MVC config, see EmplyeeNotFoundAdvice.java
-    }
-}
